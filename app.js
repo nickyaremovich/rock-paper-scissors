1f20d2bcd461ab1509cb6d5da2d7aa853c6d7a14
Nick Yaremovich <nickyaremovich@gmail.com>
2025-05-16 15:04:13 -0600

    add game logic in playRound function

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-humanScore = 0;
-computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3);
@@ -25,3 +25,31 @@ function getUserChoice() {
 
 const computerChoice = getComputerChoice();
 const userChoice = getUserChoice();
+
+function playRound(userChoice, computerChoice) {
+    console.log(`User choice: ${userChoice} | Computer choice: ${computerChoice}`)
+    if (userChoice === computerChoice) {
+        console.log('Tie!')
+    } else if (userChoice === 'rock' && computerChoice === 'paper')  {
+        console.log('User loses. Paper beats rock.')
+        computerScore++;
+    } else if (userChoice === 'rock' && computerChoice === 'scissors') {
+        console.log('User wins! Rock beats scissors.')
+        humanScore++;
+    } else if (userChoice === 'paper' && computerChoice === 'scissors') {
+        console.log('Computer wins. Scissors beats paper.')
+        computerScore++;
+    } else if (userChoice === 'paper' && computerChoice === 'rock') {
+        console.log('User wins! Paper beats rock.')
+        humanScore++;
+    } else if (userChoice === 'scissors' && computerChoice === 'paper') {
+        console.log('User wins! Scissors beats paper.')
+        humanScore++;
+    } else {
+        console.log('Computer wins. Rock beats scissors.')
+        humanScore++;
+    } 
+    console.log(`User score: ${humanScore} | Computer score: ${computerScore}`);
+};
+
+playRound(userChoice, computerChoice);
